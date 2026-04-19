5763894a6cb07f7d16391902cf9033a9b630ed92
Nathan Stitt <nathan@stitt.org>
2026-04-18 22:45:55 -0500

    chore: scaffold @tinycld/google-takeout-import package

---
+++ settings/takeout.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function TakeoutSettingsPanel() {
+    return (
+        <View>
+            <Text>Google Takeout Import</Text>
+        </View>
+    )
+}
