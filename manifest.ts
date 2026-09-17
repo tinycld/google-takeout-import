7641b863b8b7be26d0f2c2edb32fb93a1dc4cf8a
Nathan Stitt <nathan@stitt.org>
2026-09-17 15:51:26 -0500

    chore: release v0.2.1

--- manifest.ts
+++ manifest.ts
@@ -1,7 +1,7 @@
 const manifest = {
     name: 'Google Takeout Import',
     slug: 'google-takeout-import',
-    version: '0.2.0',
+    version: '0.2.1',
     description: 'Import data from Google Takeout .zip files.',
     settings: [
         {
